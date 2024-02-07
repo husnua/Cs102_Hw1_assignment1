e8156247618e0cc397b713088c5a78970f2c5fda
Mustafa Akseli <makseli2005@outlook.com>
2024-02-07 15:27:34 +0300

    Menu added

--- test.java
+++ test.java
@@ -2,6 +2,8 @@ package Cs102_Hw1_assignment1;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.random.*;
+import java.util.Arrays;
+
 
 /**
  * test
@@ -20,9 +22,58 @@ public class test {
             arr[i] = r1.nextInt(100);
         }
 
+        int choice;
+        System.out.println("Your array: " + Arrays.toString(arr));
+       do {
+        System.out.println("-----Menu-----");
+        System.out.println("1-) Find the maximum");
+        System.out.println("2-) Find the minimum");
+        System.out.println("3-) Find the average");
+        System.out.println("4-) Find the differences from the average");
+        System.out.println("5-) Find the sum of the odd indexes");
+        System.out.println("6-) Find the sum of the even indexes");
+        System.out.println("7-) Exit");
+        System.out.println("Please select an option from the menu: ");
+        choice = in.nextInt();
+
+        // Finding the Maximum
+        if(choice==1) {
+            System.out.println(ArrayOperations.findMax(arr)); 
+        }
+
+        // Finding the Minimum
+        else if (choice == 2) {
+            System.out.println(ArrayOperations.findMin(arr));
+        }
+
+        // Finding the Average
+        else if (choice==3) {
+            System.out.println(ArrayOperations.findAverage(arr));
+        }
+
+        // Finding the Differences from the Average
+        else if (choice==4) {
+            System.out.println(Arrays.toString(ArrayOperations.differences(arr)));
+        }
+
+        else if (choice == 5) {
+            System.out.println(ArrayOperations.sumOfOdds(arr));
+        }
+
+        else if (choice == 6) {
+            System.out.println(ArrayOperations.sumOfEvens(arr));
+        }
+        
+        else if (choice == 7) {
+            System.out.println("Exiting the menu.");
+        }
+        
+       } while (choice != 7);
+
+
+
+
+       in.close();
         
     }
-
-    
-
 }
